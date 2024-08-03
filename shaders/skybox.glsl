81623bb8fd749dc48a83538013949a57141a2851
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2024-08-03 22:58:56 +0200

    mie scattering

--- shaders/skybox.glsl
+++ shaders/skybox.glsl
@@ -24,11 +24,31 @@ void main() {
 
 #ifdef FragmentShader ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#include "../grax/shaders/app.glsl"
+
 out vec4 FragColor;
+
+uniform vec3 sun_dir;
+uniform vec3 sun_radiance;
 
 void main() {
     vec3 sky_dir = normalize(v2f.pos);
-    FragColor = vec4(abs(sky_dir), 1.0);
+
+    float fCos = dot(sun_dir, -sky_dir);
+
+    float g = -0.95;
+    float g2 = g*g;
+    float MiePhase = 1.5 * ((1.0 - g2) / (2.0 + g2)) * (1.0 + fCos*fCos) / pow(1.0 + g2 - 2.0*g*fCos, 1.5);
+
+    vec3 sky_color = vec3(0.1, 0.1, 1);
+
+    FragColor = vec4(sky_color + vec3(MiePhase), 1.0);
+
+    // FragColor = vec4(abs(sky_dir), 1.0);
 }
 
 #endif
+
+
+
+// FragColor.rgb = acc_color * (v3InvWavelength * fKrESun + vec3(fMiePhase * fKmESun));
