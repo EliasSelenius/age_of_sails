711b6bd8fef469f55444daf2c2d048444d9a9c51
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2025-10-26 15:44:58 +0100

    added terrain textures (triplanar mapping)

--- shaders/ground.glsl
+++ shaders/ground.glsl
@@ -1,26 +1,5 @@
 
 vec4 get_terrain(sampler2D height_map, vec2 a_Uv) {
-    // float region_size = 128;
-    // float height_map_res_downscale = 4;
-
-    // float height_map_res = region_size / height_map_res_downscale + 2.0;
-
-    // float r = 1.0 / height_map_res;
-    // vec2 uv = r + a_Uv * (height_map_res - 2) / height_map_res;
-
-    // float g = 1.0 / (region_size+1);
-    // float h1 = texture(height_map, uv).x;
-    // float h2 = texture(height_map, uv + vec2(g, 0)).x;
-    // float h3 = texture(height_map, uv + vec2(0, g)).x;
-    // float h4 = texture(height_map, uv + vec2(-g, 0)).x;
-    // float h5 = texture(height_map, uv + vec2(0, -g)).x;
-
-    // float d = (region_size+1) * g;
-    // vec3 p1 = vec3(0,  h1,  0);
-    // vec3 p2 = vec3(d,  h2,  0);
-    // vec3 p3 = vec3(0,  h3,  d);
-    // vec3 p4 = vec3(-d, h4,  0);
-    // vec3 p5 = vec3(0,  h5, -d);
 
     /*
         p3
--- shaders/ground_mesh.glsl
+++ shaders/ground_mesh.glsl
@@ -1,25 +1,71 @@
 
+#extension GL_ARB_bindless_texture : require
+
 IO FragData {
     vec3 pos;
     vec3 normal;
+    vec3 view_pos;
+    vec3 view_normal;
     vec2 uv;
     float height;
 } v2f;
 
 #include "../grax/shaders/noise.glsl"
 
+uniform vec2 chunk_pos;
+uniform vec4 u_color_tint = vec4(0.0);
+
+layout (binding = 0) uniform sampler2D height_map;
+layout (binding = 1) uniform sampler2D texture_albedo_sand;
+layout (binding = 2) uniform sampler2D texture_albedo_grass;
+layout (binding = 3) uniform sampler2D texture_albedo_cliff;
+
+
 #ifdef VertexShader /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "../grax/shaders/camera.glsl"
 #include "shaders/ground.glsl"
 
-uniform vec2 chunk_pos;
-uniform sampler2D height_map;
-
 layout (location = 0) in vec3 a_Pos;
 layout (location = 1) in vec3 a_Normal;
 layout (location = 2) in vec2 a_Uv;
 
+vec4 noise_test(vec2 coord) {
+
+    float h = 0;
+    vec2 grad = vec2(0.0);
+    for (int i = 1; i <= 4; i++) {
+        float f = 1000.0 / float(i);
+        float a = 500.0 / float(i);
+
+        vec3 n = noised2(coord / f);
+        h += a*n.z;
+        grad += (a/f)*n.xy;
+    }
+
+    vec3 norm = normalize(vec3(-grad.x, 1, -grad.y));
+    return vec4(norm, h);
+
+
+
+
+    // float f0 = 1000.0;
+    // float f1 = 100.0;
+
+    // vec2 q0 = coord / f0;
+    // vec2 q1 = coord / f1;
+    // float a0 = 300;
+    // float a1 = 90;
+
+    // vec3 n0 = noised2(q0);
+    // vec3 n1 = noised2(q1);
+
+    // float h = a0*n0.z + a1*n1.z;
+    // vec2 grad = (a0/f0)*n0.xy + (a1/f1)*n1.xy;
+
+    // vec3 norm = normalize(vec3(-grad.x, 1, -grad.y));
+    // return vec4(norm, h);
+}
 
 void main() {
 
@@ -27,18 +73,23 @@ void main() {
     vec3 normal = terrain.xyz;
     float height = terrain.w;
 
-    vec4 pos = vec4(a_Pos, 1.0);
-    pos.xz += chunk_pos;
-    pos.y = height;
-    pos = camera.view * pos;
+    vec4 pos = vec4(a_Pos, 1.0) + vec4(chunk_pos.x, height, chunk_pos.y, 0);
+
+    vec4 n = noise_test(pos.xz);
+    pos.y = n.w;
+    normal = n.xyz;
+
+    vec4 view_pos = camera.view * pos;
+    vec3 view_normal = mat3(camera.view) * normal;
 
     v2f.pos = pos.xyz;
-    v2f.normal = mat3(camera.view) * normal;
+    v2f.normal = normal;
+    v2f.view_pos = view_pos.xyz;
+    v2f.view_normal = view_normal;
     v2f.uv = a_Uv;
-    v2f.height = height;
+    v2f.height = n.w; // height;
 
-
-    gl_Position = camera.projection * pos;
+    gl_Position = camera.projection * view_pos;
 }
 #endif
 
@@ -83,18 +134,52 @@ layout (location = 0) out vec4 FragPos_Metallic;
 layout (location = 1) out vec4 FragNormal_Roughness;
 layout (location = 2) out vec3 FragColor;
 
-uniform vec3 u_color_additive = vec3(1.0);
 
 void main() {
-    FragPos_Metallic.xyz = v2f.pos;
+    FragPos_Metallic.xyz = v2f.view_pos;
     FragPos_Metallic.w   = 0;
 
-    FragNormal_Roughness.xyz = normalize(v2f.normal);
+    vec3 normal = normalize(v2f.normal);
+    vec3 view_normal = normalize(v2f.view_normal);
+    FragNormal_Roughness.xyz = view_normal;
     FragNormal_Roughness.w   = 0.9;
 
-    float contour = 1.0 - step(0.01, fract(v2f.height));
-    FragColor = vec3(0.0); //vec3(contour);// + vec3(v2f.uv, 0.0);
+    sampler2D texture_albedo;
 
-    FragColor += u_color_additive;
+    if (v2f.height < 10) {
+        texture_albedo = texture_albedo_sand;
+    } else {
+        texture_albedo = texture_albedo_grass;
+    }
+
+    vec2 cliff_uv_x = vec2(v2f.pos.x, v2f.pos.y) / 100.0;
+    vec2 cliff_uv_z = vec2(v2f.pos.z, v2f.pos.y) / 100.0;
+    vec3 cliff_albedo_x = texture(texture_albedo_cliff, cliff_uv_x).rgb;
+    vec3 cliff_albedo_z = texture(texture_albedo_cliff, cliff_uv_z).rgb;
+    float t_x = abs(dot(vec3(1,0,0), normal));
+    float t_z = abs(dot(vec3(0,0,1), normal));
+
+
+    vec3 cliff_albedo = cliff_albedo_x*t_x + cliff_albedo_z*t_z;
+
+    vec3 ground_albedo = texture(texture_albedo, v2f.uv).rgb;
+    float t_y = abs(dot(vec3(0,1,0), normal));
+    FragColor = cliff_albedo + ground_albedo*t_y;
+
+
+    float contour = 1.0 - step(0.01, fract(v2f.height));
+    // FragColor += vec3(contour);
+
+    FragColor = mix(FragColor, u_color_tint.rgb, u_color_tint.a);
+
+    bvec3 na = isnan(FragColor);
+    if (na.x || na.y || na.z) {
+        FragColor = vec3(1,0,0);
+    }
+
+    bvec3 nf = isinf(FragColor);
+    if (nf.x || nf.y || nf.z) {
+        FragColor = vec3(0,1,0);
+    }
 }
 #endif
