abab7725efc2fc35689a379a03ef7b06fa449070
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2026-08-23 17:57:28 +0200

    better map contours

--- shaders/map.glsl
+++ shaders/map.glsl
@@ -5,6 +5,8 @@
 #include "shaders/ground.glsl"
 
 uniform mat3 camera_matrix = mat3(1.0);
+uniform float u_contour_width = 0.85;
+uniform vec4  u_contour_color = vec4(0.0, 0.0, 0.0, 0.2);
 
 #define FragData_Block FragData {\
     vec2 coord;\
@@ -38,6 +40,7 @@ void main() {
     float height = ter.w;
     vec3 normal = ter.xyz;
 
+    // float light = mix(0.5, 1.0, dot(normal, vec3(0, 0, 1)));
     float light = dot(normal, vec3(0, 1, 0));
 
     vec3 color_shallow = vec3(0.1, 0.4, 0.7);
@@ -49,10 +52,15 @@ void main() {
         color = mix(color_water, color_shallow, exp(height / 100));
     }
 
-    float contour = smoothstep(0.95, 1.0, fract(height));
-
     color *= light;
 
-    FragColor = vec4(mix(color, vec3(0), contour), 1.0);
+    float zoom = camera_matrix[0][0];
+    float e = u_contour_width;
+    float t = abs(fract(height) - 0.5)*2.0;
+    float contour = smoothstep(e, 1.0, t);// * clamp(30.0 / zoom, 0, 1);
+
+    vec3 contour_color = mix(color, u_contour_color.rgb, u_contour_color.a);
+
+    FragColor = vec4(mix(color, contour_color, contour), 1.0);
 }
 #endif
